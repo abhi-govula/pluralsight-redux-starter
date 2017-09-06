f9e60570124e0a5fad981af61428156b3a59124e
Abhi Govula <abhi.govula@scriptbees.com>
2017-09-06 17:21:37 +0530

    Adds Basic Redux Todo App

--- src/store.js
+++ src/store.js
@@ -12,7 +12,7 @@ const logger = store => next => action => {
   console.log('dispatching', action)
   let result = next(action)
   console.log('next state', store.getState())
-  return result
+  return result;
 }
 
 const middleware = applyMiddleware( logger, thunk);
